1d4827ae2d12c9632898a0acd69b0ee30f964c5f
Mushfiqur000 <2022731073@student.sust.edu>
2026-09-09 20:39:07 +0600

    Update app.js

--- app.js
+++ app.js
@@ -248,7 +248,6 @@ window.trackMetaEvent = async function(eventName, userData = {}, customData = {}
                         event_id: eventId, // Deduplication Key
                         action_source: "website",
                         event_source_url: window.location.href,
-                        test_event_code: "TEST82777",
                         user_data: userData,
                         custom_data: customData
                     }
